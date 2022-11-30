59abe83f72f81e1d7c6ce9fc2bea2e42f4a8f2d7
Brian <58400498+nairb725@users.noreply.github.com>
2022-11-30 11:19:48 +0100

    add/addmany/change

--- lib/mongodb.dart
+++ lib/mongodb.dart
@@ -10,6 +10,43 @@ class MongoDatabase {
     var status = db.serverStatus();
     print(status);
     var collection = db.collection(COLLECTION_NAME);
+
+
+    //add one
+    /*
+    await collection.insertOne({
+      "usename" : "mp",
+      "name" : "Spank maiyasse",
+      "email" : "dfghjkl@gmail.com"
+    });*/
+
+
+
+    //add many
+    /*await collection.insertMany([
+      {
+        "usename": "mp1",
+        "name": "Spank1 maiyasse1",
+        "email": "dfghjkl@gmail.com1"
+      },
+      {
+        "usename": "mp2",
+        "name": "Spank2 maiyasse2",
+        "email": "dfghjkl@gmail.com2"
+      },
+      {
+        "usename": "mp3",
+        "name": "Spank3 maiyasse3",
+        "email": "dfghjkl@gmail.com3"
+      },
+    ]);*/
+
+    print(await collection.find().toList());
+
+
+    
+    //update document
+    await collection.update(where.eq('usename', 'mp'), modify.set('name', 'Findus Ecurry'));
     print(await collection.find().toList());
   }
 }
