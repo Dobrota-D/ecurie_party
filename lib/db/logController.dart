1ff5e07c0731b032aaf171b8f8bc7344ffe384b6
Brian <58400498+nairb725@users.noreply.github.com>
2022-12-01 15:58:27 +0100

    1/2 logcontrolller

--- lib/db/logController.dart
+++ lib/db/logController.dart
@@ -1,9 +1,25 @@
+import '../models/log.dart';
 import 'database.dart';
+
 
 class logController {
   static final String collectionName = "log";
 
-  static void getLog(DataBase db, int idLog){
-    db.get(collectionName, {"id": idLog});
+  static Log getLog(DataBase db, int idLog){
+   return logController._logController(db.get(collectionName, {"id": idLog}));
+
+  }
+
+  static List<Log> getListLog(DataBase db){
+    List<Log> logs = [];
+    List<Map<String, dynamic>> listLog = db.getCollection(collectionName);
+    for (var log in listLog){
+      logs.add(_logController(log));
+    }
+    return logs;
+  }
+
+  static Log _logController(Map<String, dynamic> data){
+    return Log.init(data);
   }
 }
--- lib/models/log.dart
+++ lib/models/log.dart
@@ -8,4 +8,9 @@ class Log extends DbCollection{
   String message;
 
   Log(this.id, this.date, this.title, this.message);
+
+  static Log init(Map<String, dynamic> data){
+    return Log(data["id"], data["date"], data["title"],
+        data["message"]);
+  }
 }
